3ef86925de1ea41ff2390ee6506560d486b74004
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-11-11 20:11:20 +0900

    アプリ完成

--- Udemy_sec5/Udemy_sec5/ViewController.swift
+++ Udemy_sec5/Udemy_sec5/ViewController.swift
@@ -7,31 +7,75 @@
 //
 
 import UIKit
-
+//
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    //var timer:Timer
+    var timer = Timer()
+    var count = Int()
+    var imageArray = [UIImage]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //startボタンを押せないようにする
+        
+        count = 0
+        //stopボタンを押せないようにする、プロパティ
+        stopButton.isEnabled = false
+        
+        //
+        for X in 0..<5 {
+            print(X)
+            let image = UIImage(named: "\(X)")
+            imageArray.append(image!)
+            
+        }
+        
+        imageView.image = UIImage(named: "0")
+    }
+    
+    func startTimer(){
+        //タイマーを回す　０.２秒毎にTimerUpdateメソッド呼ぶ
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    
+    //0.2秒毎に呼ばれる
+    @objc func timerUpdate() {
+        count = count + 1
+        //６枚目がない以上countで５が出ないよう、４で抑えられるようにする
+        if count > 4 {
+            count = 0
+        }
+        imageView.image = imageArray[count]
+        
+        //imageArray[]
         
     }
-
+    
     @IBAction func start(_ sender: Any) {
         
         //imageViewのimageに画像反映
-        
-        //start押すと押せなくなる
+        startTimer()
+        //start押すと押せなくなる,stopが押せる
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
     }
     
     @IBAction func stop(_ sender: Any) {
-       
+        
         //imageに反映されている画像の流れをストップする
         
-        //startボタンが押せるようになる
-        
+        //startボタンが押せるようになる、stopが押せない
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
+        //タイマーストップ
+        timer.invalidate()
     }
-    
 }
 
