a776b24ab4db441bb6ffe576ed5309e85d02a25c
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-11-14 13:17:57 +0900

    解説挿入

--- Udemy_sec5/Udemy_sec5/ViewController.swift
+++ Udemy_sec5/Udemy_sec5/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//
+//継承 , weak、弱参照（相互参照解決）
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         //stopボタンを押せないようにする、プロパティ
         stopButton.isEnabled = false
         
-        //
+        //   \
         for X in 0..<5 {
             print(X)
             let image = UIImage(named: "\(X)")
@@ -41,11 +41,11 @@ class ViewController: UIViewController {
     }
     
     func startTimer(){
-        //タイマーを回す　０.２秒毎にTimerUpdateメソッド呼ぶ
+        //タイマーを回す　０.２秒毎にTimerUpdateメソッド呼ぶ,Timer省略可能,scheduledTimer
         timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
     }
     
-    //0.2秒毎に呼ばれる
+    //0.2秒毎に呼ばれる、@objc （）
     @objc func timerUpdate() {
         count = count + 1
         //６枚目がない以上countで５が出ないよう、４で抑えられるようにする
@@ -57,7 +57,7 @@ class ViewController: UIViewController {
         //imageArray[]
         
     }
-    
+    //sender(送信者、押されたボタンの判別),@IBAction（UI部品をメソッドとして接続する。ここを押すことで可能な動作をここに）
     @IBAction func start(_ sender: Any) {
         
         //imageViewのimageに画像反映
@@ -71,10 +71,10 @@ class ViewController: UIViewController {
         
         //imageに反映されている画像の流れをストップする
         
-        //startボタンが押せるようになる、stopが押せない
+        //startボタンが押せるようになる、stopが押せない、isEnabled（有効である）
         startButton.isEnabled = true
         stopButton.isEnabled = false
-        //タイマーストップ
+        //タイマーストップ,invalidate（無効にする、）(タイマー破棄、停止)
         timer.invalidate()
     }
 }
